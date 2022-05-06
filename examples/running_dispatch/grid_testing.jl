0ebecd13507f9ad246f208497925a0c0cc826181
Philipp Eitner <eitner@mpia.de>
2022-05-06 10:43:12 +0200

    added namlist reader + writer

--- examples/running_dispatch/grid_testing.jl
+++ examples/running_dispatch/grid_testing.jl
@@ -3,5 +3,4 @@ using MUST
 
 central_seeds = Dict("tt_K" => 0.6204e4,       "d_cgs" => 1.6397e-7,        "ee_min" => 5.5)
 limits        = Dict(p => [central_seeds[p]-0.1*central_seeds[p], central_seeds[p]+0.1*central_seeds[p]] for p in keys(central_seeds))
-
-grid = randrange(limits)
+grid          = Dict(p => MUST.randrange(limits[p]) for p in keys(central_seeds))
