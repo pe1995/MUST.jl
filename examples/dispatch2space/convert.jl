046a5bd72fb62dfea47372166faffaf307197756
Philipp Eitner <eitner@mpia.de>
2022-06-05 11:03:34 +0200

    bug fixes + teff in convert

--- examples/dispatch2space/convert.jl
+++ examples/dispatch2space/convert.jl
@@ -21,6 +21,7 @@ end
     using Glob
     using PyCall
     using Distributed
+    using Interpolations
 end
 
 @everywhere begin
@@ -30,9 +31,6 @@ end
     # Different EOS
     #const eos        = dispatch.EOS.bifrost(top=MUST.dispatch_location, dir="data/eos/stagger/");
     #const eos_legacy = dispatch.EOS.stagger(top=MUST.dispatch_location, file="table.dat");
-
-    # Use the new Squaregas EOS 
-    eos = MUST.SquareGasEOS(MUST.@in_dispatch("input_data/square_gas_1"));
 end
 
 folder = MUST.@in_dispatch ARGS[1]
@@ -43,8 +41,29 @@ else
     snapshots         = sort(MUST.list_of_snapshots(content_of_folder))
 end
 
-# Send the data to the worksers
-MUST.sendsync(workers(), folder=folder)
+# Name of the namelist of the current folder
+nml_name = MUST.@in_dispatch splitpath(folder)[end]
+
+# Init namelist
+const nml = MUST.StellarNamelist(nml_name*".nml")
+
+# Use the new Squaregas EOS 
+eos_path = replace(nml.eos_params["table_loc"], "'"=>"")
+@show eos_path
+const eos_sq = MUST.SquareGasEOS(MUST.@in_dispatch(eos_path))
+
+# Effective temperature present?
+save_info = ispath(joinpath(folder, "teff.dat")) 
+if save_info
+    # Read and interpolate teff data
+    teff_func = MUST.teff_interpolated(joinpath(folder, "teff.dat"))
+    
+    # Send the data to the worksers
+    MUST.sendsync(workers(), teff=teff_func)
+end
+
+# Send the data to the workers
+MUST.sendsync(workers(), folder=folder, do_teff=save_info, ini_nml=nml, eos=eos_sq)
 
 @everywhere function convert_snapshots(snapshots)
     for i_s in 1:length(snapshots)
@@ -66,6 +85,12 @@ MUST.sendsync(workers(), folder=folder)
             # Add additional columns already in CGS after converting
             MUST.add_from_EOS!(s, eos, :T)
             MUST.add_from_EOS!(s, eos, :kr)
+
+            # Check if Teff should be added
+            if do_teff
+                @info "setting teff for sn $(snapshots[i_s])"
+                MUST.set!(s.parameter, teff, ini_nml)
+            end
 
             # Also save the snapshot as Box (a regular gridded 3D-cube) to save time later
             b_s = MUST.Box(s)
@@ -95,8 +120,6 @@ wrk     = sort(workers())
 for (i,split) in enumerate(MUST.split_similar(snapshots, nworkers()))
     # Send the split to the worker
     MUST.sendsync(wrk[i], split_l=split)
-
-    @show split wrk[i]
 
     # Execute the function convert_snapshots on the passed split
     append!(futures, [@spawnat wrk[i] convert_snapshots(split_l)])
