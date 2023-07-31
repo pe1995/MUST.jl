e2bc5d0e9ab7ede80876474ab22eb5bc22e48d2a
Philipp Eitner <eitner@mpia.de>
2023-07-31 17:14:12 +0200

    add average stagger models

---
+++ examples/initial_models/copy_over.jl
@@ -0,0 +1,6 @@
+folder_to_copy = ARGS[1]
+nml = joinpath(folder_to_copy, "ininml.dat")
+
+name = folder_to_copy[end] == '/' ? folder_to_copy[1:end-1] : folder_to_copy
+
+cp(nml, joinpath("/home/eitner/shared/model_grid/stellar_atmospheres/", "$(name).nml"), force=true)
