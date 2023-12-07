595cdf287dbd36184f534727cb86ed5e45f2372b
Philipp Eitner <eitner@mpia.de>
2023-12-07 08:56:35 +0000

    fix bug

--- examples/initial_models/copy_over_cloud.jl
+++ examples/initial_models/copy_over_cloud.jl
@@ -5,8 +5,8 @@ folder_to_copy = glob("DIS_MARCS_E*/")
 
 nml = joinpath.(folder_to_copy, "ininml.dat")
 name = [f[end] == '/' ? f[1:end-1] : f for f in folder_to_copy]
-name = [first(split(name, "DIS_MARCS_E_", keepempty=false)) for n in name]
-name = [first(split(name, "_", keepempty=false)) for n in name]
+name = [first(split(n, "DIS_MARCS_E_", keepempty=false)) for n in name]
+name = [first(split(n, "_", keepempty=false)) for n in name]
 
 for (i, n) in enumerate(name)
     cp.(nml[i], joinpath(path_dispatch, "grid_$(n).nml"), force=true)
